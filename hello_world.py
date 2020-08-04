2c008268608f2018e3aa62b24eb346cbdba2f183
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-08-04 19:52:28 +0000

    Added in hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on August 2020
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
